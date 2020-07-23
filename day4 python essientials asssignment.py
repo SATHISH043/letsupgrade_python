ca4888ee16f5406e933b37fa73fc91c1b4f5edac
SATHISH043 <68598448+SATHISH043@users.noreply.github.com>
2020-07-23 18:23:45 +0530

    Add files via upload

---
+++ day4 python essientials asssignment.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ASSIGNMENT 1 (DAY 4 PYTHON ESSENTIALS)
+
+# In[2]:
+
+
+# Python code to find occurrences 
+# of a substring in a string 
+
+# defining a string
+str1 = "I live in India, India is a great country"
+
+# finding occurrences of 'India'
+# in complete string
+print(str1.count("India"))
+
+# finding occurrences of 'India'
+# from 0 to 15 index
+print(str1.count("India",0,15))
+
+
+# ASSIGNMENT 2
+
+# In[3]:
+
+
+string = 'sathishkumar'
+
+print(string.isupper()) 
+  
+
+string = 'SATHISHKUMAR'
+
+print(string.islower())
+
+string = 'hariharan'
+
+print(string.islower())
+
+
+# In[ ]:
+
+
+
+
