413010fde0b7c072a2b927c4bd2429d06169c37c
SATHISH043 <68598448+SATHISH043@users.noreply.github.com>
2020-07-27 22:43:34 +0530

    Add files via upload

---
+++ day6_python essentials.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+list1=[1,2,3,4,5,6]
+list2=["a","b","c","d","e","f"]
+res = {list1[i]: list2[i] for i in range(len(list1))}
+print(res)
+
+        
